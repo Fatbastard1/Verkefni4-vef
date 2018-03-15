cc89996a4e2d073e9607330a5f70d6a0acfcefeb
Fatbastard1 <arnihl@gmail.com>
2018-03-15 23:50:27 +0000

    La

--- scraper.js
+++ scraper.js
@@ -1,8 +1,26 @@
 require('dotenv').config();
 require('isomorphic-fetch');
 const cheerio = require('cheerio');
+const redis = require('redis');
+const util = require('util');
+
+const redisOptions = {
+  url: 'redis://127.0.0.1:6379/0'
+};
+
+const client = redis.createClient(redisOptions);
+
+const cacheTimeToLive = 10000000;
+
+const asyncGet = util.promisify(client.get).bind(client);
+const asyncSet = util.promisify(client.set).bind(client);
+const asyncflush = util.promisify(client.flushall).bind(client);
+
+
 
 /* todo require og stilla dót */
+
+
 
 /**
  * Listi af sviðum með „slug“ fyrir vefþjónustu og viðbættum upplýsingum til
@@ -31,6 +49,21 @@ const departments = [
   },
 ];
 
+async function get(url, cacheKey) {
+  const cached = await asyncGet(cacheKey);
+
+  if(cached){
+    return cached
+  }
+
+  const response = await fetch(url);
+  const text = await response.text();
+
+  await asyncSet(cacheKey, text, 'EX', cacheTimeToLive);
+
+  return text;
+}
+
 /**
  * Sækir svið eftir `slug`. Fáum gögn annaðhvort beint frá vef eða úr cache.
  *
@@ -39,6 +72,60 @@ const departments = [
  */
 async function getTests(slug) {
   /* todo */
+  let i = 0;
+  for (i = 0; i < departments.length; i++) {
+    if(departments[i].slug === slug){
+      //console.log(i);
+      break;
+    }
+  }
+  const response = await get('https://ugla.hi.is/Proftafla/View/ajax.php?sid=2027&a=getProfSvids&proftaflaID=37&svidID='
+   + (i+1) + '&notaVinnuToflu=0', slug);
+
+  //console.log(response);
+
+  const $ = cheerio.load(JSON.parse(response).html, {
+    normalizeWhitespace: true,
+    xmlMode: true,
+});
+
+
+
+    const tablesdata = $('.box h3');
+
+  //console.log(tablesdata.text());
+
+    const tables = [];
+    const tafladata = [];
+
+    
+    tablesdata.each((i, el) => {
+    const title = $(el).eq(i).text().trim();
+    //console.log(title);
+    const tafla = $(title).find('tbody');
+
+
+
+    tafla.children().each((row) => {
+      const taflael = $(row).children();
+      tafladata.push({
+        course: $(taflael[0]).text(),
+        name: $(taflael[1]).text(),
+        type: $(taflael[2]).text(),
+        students: $(taflael[3]).text(),
+        date: $(taflael[4]).text(),
+        });
+    });
+    tables.push({
+      header: tablesdata.text().trim(),
+      tests: tafladata,
+    });
+  });
+
+  return tables;
+
+  // console.log(table);
+  // client.quit();
 }
 
 /**
@@ -48,6 +135,8 @@ async function getTests(slug) {
  */
 async function clearCache() {
   /* todo */
+  asyncFlush();
+
 }
 
 /**
@@ -57,6 +146,7 @@ async function clearCache() {
  */
 async function getStats() {
   /* todo */
+
 }
 
 module.exports = {
